e322dc50aa3fa947c54889ca589dd52187db87f5
Robert Berry <rjberry@gmail.com>
2013-11-20 09:21:48 +0000

    Add Either trait

--- src/main/scala/com/github/robertberry/fpis/Chapter4.scala
+++ src/main/scala/com/github/robertberry/fpis/Chapter4.scala
@@ -86,4 +86,34 @@ object Chapter4 {
   }
 
   def sequence2[A](as: List[Maybe[A]]): Maybe[List[A]] = traverse(as)(identity)
+
+  /** Exercise 6
+    *
+    * Implement map, flatMap, orElse and map2 for Either
+    */
+  sealed trait Either2[+E, +A] {
+    def map[B](f: A => B): Either2[E, B] = this match {
+      case Right2(a) => Right2(f(a))
+      case Left2(err) => Left2(err)
+    }
+
+    def flatMap[EE >: E, B](f: A => Either2[EE, B]): Either2[EE, B] = this match {
+      case Left2(err) => Left2(err)
+      case Right2(a) => f(a)
+    }
+
+    def orElse[EE >: E, B >: A](b: => Either2[EE, B]): Either2[EE, B] = this match {
+      case _: Left2 => this
+      case _ => b
+    }
+
+    def map2[EE >: E, B, C](b: Either2[EE, B])(f: (A, B) => C): Either2[EE, C] = (this, b) match {
+      case (Right2(a), Right2(b)) => Right2(f(a, b))
+      case (Left2(err), _) => Left2(err)
+      case (_, Left2(err)) => Left2(err)
+    }
+  }
+
+  case class Left2[+E](value: E) extends Either2[E, Nothing]
+  case class Right2[+A](value: A) extends Either2[Nothing, A]
 }
